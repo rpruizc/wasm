1038bd91aafb5fe8d530d4bce28ab47d0bf89258
Rodolfo Ruiz <rpruiz@me.com>
2020-05-17 13:16:17 -0500

    set, get, and range for a piece

--- wasmcheckers/func_test.js
+++ wasmcheckers/func_test.js
@@ -14,4 +14,11 @@ fetch('./func_test.wasm').then(response =>
   var offset = instance.exports.offsetForPosition(3, 4);
   console.log("Offset for 3, 4 is: ", offset);
 
+  console.debug("White is white?", instance.exports.isWhite(white));
+  console.debug("Black is black?", instance.exports.isBlack(black));
+  console.debug("Black is white?", instance.exports.isWhite(black));
+  console.debug("Uncrowned white", instance.exports.isWhite(instance.exports.withoutCrown(crowned_white)));
+  console.debug("Uncrowned black", instance.exports.isBlack(instance.exports.withoutCrown(crowned_black)));
+  console.debug("Crowned is crowned", instance.exports.isCrowned(crowned_black));
+  console.debug("Crowned is crowned(b)", instance.exports.isCrowned(crowned_white));
 });
