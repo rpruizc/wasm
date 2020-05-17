16378a91a213c2ed585723b27dce115bb9c9a933
Rodolfo Ruiz <rpruiz@me.com>
2020-05-17 09:42:42 -0500

    Testing scripts and web page

---
+++ README.md
@@ -0,0 +1,5 @@
+Run an HTTP server for testing, ej
+
+'''
+python3 -m http.server
+'''
---
+++ wasmcheckers/func_test.js
@@ -0,0 +1,17 @@
+fetch('./func_test.wasm').then(response =>
+  response.arrayBuffer()
+).then(bytes=>WebAssembly.instantiate(bytes)).then(results=>{
+  console.log("Loaded wasm module");
+  instance = results.instance;
+  console.log("instance", instance);
+
+  var white = 2;
+  var black = 1;
+  var crowned_white = 6;
+  var crowned_black = 5;
+
+  console.log('Calling offset');
+  var offset = instance.exports.offsetForPosition(3, 4);
+  console.log("Offset for 3, 4 is: ", offset);
+
+});
